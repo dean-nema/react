34f7cfadf8417a57dc56137f49649b6af2d2d177
dean-nema <nemarambakundayi@gmail.com>
2023-07-20 23:54:00 +0200

    props, useState and installed tailwind css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 
 import './App.css';
 import Employee from './components/employee';
-
+import { useState } from 'react';
 function App() {
-   console.log('we are about to list the employees');
-   const  showEmployees = 0;
+  const [role, setRole] = useState("dev");//
+   const  showEmployees = 1;
 return (
   <div className="App">
     {showEmployees ? 
       <>
-      <Employee/>
-      <Employee/>
-      <Employee/>
-      <Employee/>
+      <input type="text" onChange={(e) => {
+        // console.log(e.target.value);
+        setRole(e.target.value);//e.target.value captures the value entered in the input
+      }} />
+      <Employee name = "Dean" role = "Intern"/>
+      <Employee name = "Abby" role = {role}/>
+      <Employee name = "John"/>
+      
       </>
     : 
       <p>You can not see the employees</p>
