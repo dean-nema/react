8423bec9f47532047fdf7a19934a0486795d50cc
dean-nema <139811805+dean-nema@users.noreply.github.com>
2023-08-24 15:43:12 +0200

    passing a component from app.js
    here I moved i placed a variable in app.js that will hold the editEmployee component and used that variable inside employee file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Employee from './components/employee';
 import { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
 import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
 function App() {
   const [employees, setEmployees] = useState(
     [
@@ -69,13 +70,21 @@ return (
     <>
       <div className="flex flex-wrap justify-center" >
       {employees.map((employee)=>{
+        const editEmployee = ( 
+        <EditEmployee 
+        id={employee.id}
+        name={employee.name}
+        role={employee.role} 
+        updateEmployee={updateEmployee}
+        />
+        );
            return (<Employee
             key={employee.id}
             id={employee.id}
             name={employee.name}
             role={employee.role}
             img={employee.img}
-            updateEmployee = {updateEmployee}
+           editEmployee = {editEmployee}
           />
          )
       })}
