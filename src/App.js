e4a19f14aac45373351ffb746e3cbd1d8eb68ae9
dean-nema <139811805+dean-nema@users.noreply.github.com>
2023-08-28 19:14:59 +0200

    page routing and structuring

--- src/App.js
+++ src/App.js
@@ -5,96 +5,15 @@ import { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
-function App() {
-  const [employees, setEmployees] = useState(
-    [
-      {
-        id:1,
-        name:"Dean",
-        role:"Developer",
-        img: require("./pictures/Rogue.png")
-      },
-      {
-        id:2,
-        name:"James",
-        role:"Mage",
-        img: require("./pictures/Mage.png")
-      },
-      {
-        id:3,
-        name:"John",
-        role:"Hunter",
-        img: require("./pictures/Hunter.jpg")
-      },
-      {
-        id:4,
-        name:"Charles",
-        role:"Warrior",
-        img: require("./pictures/Warrior.jpg")
-      },
-      {
-        id:5,
-        name:"Dani",
-        role:"Troll",
-        img: require("./pictures/Troll.jpg")
-      },
-      {
-        id:6,
-        name:"Anesu",
-        role:"Hero",
-        img: require("./pictures/Screenshot from 2023-03-17 14-21-14.png")
-      }
-    ]);
-    function updateEmployee(id,newName, newRole){
-      let updatedEmployees = employees.map((employee)=>{
-          if(id === employee.id){
-             return {...employee, name:newName, role:newRole}
-          }
-          return employee;
-      });
-      setEmployees(updatedEmployees);
-    }
-    function newEmployee(name, role, img){
-      let newEmployee = {
-        id: uuidv4(),
-        name: name,
-        role: role,
-        img: require('./pictures/'+img+'.png')
-      }
-      setEmployees([...employees, newEmployee])
-    }
-   const  showEmployees = 1;
-return (
-  <div className="App ">
-    {showEmployees ? 
-    <>
-      <div className="flex flex-wrap justify-center" >
-      {employees.map((employee)=>{
-        const editEmployee = ( 
-        <EditEmployee 
-        id={employee.id}
-        name={employee.name}
-        role={employee.role} 
-        updateEmployee={updateEmployee}
-        />
-        );
-           return (<Employee
-            key={employee.id}
-            id={employee.id}
-            name={employee.name}
-            role={employee.role}
-            img={employee.img}
-           editEmployee = {editEmployee}
-          />
-         )
-      })}
-      </div>
-      <AddEmployee newEmployee = {newEmployee}/>
-    </>
-    : 
-      <p>You can not see the employees</p>
-    }
-  </div>
+import Header from './components/header';
+import Employees from './pages/Employees';
+function App(){
+return ( <Header>
+
+         <Employees/>
+         
+
+      </Header>
 );
 }
 
